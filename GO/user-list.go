b00d508046b139c8ec94b5be3622ba80f80a1aa4
BrainstemStudio <mail@brainstemstudio.com>
2019-03-29 18:47:31 +0200

    Update user-list.go

--- GO/user-list.go
+++ GO/user-list.go
@@ -13,7 +13,7 @@ func main() {
 
 	payload := strings.NewReader("{\"broadcast_ID\" : 45}")
 
-	req, _ := http.NewRequest("POST", cfg, payload)
+	req, _ := http.NewRequest("POST", url, payload)
 
 	req.Header.Add("Content-Type", "application/json")
 	req.Header.Add("Authorization", "Basic QVAzMy1GR1BWLTYzT04tNTk3MzoxZGU1ODBlMmY2ZjcxOTk2OWY3ZjA3NTI0ZDcyYzI5Ny40OTM5NDE4NjY0ZWRhYjg0ZDVkMTNmZDNhYTM5ODQ5Yg==")
